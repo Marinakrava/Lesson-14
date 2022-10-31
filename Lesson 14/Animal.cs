ebb1b801e1838656e895f85cdd538bae8571cbd6
yxa.kravchenko@yandex.ru <yxa.kravchenko@yandex.ru>
2022-10-31 23:39:53 +0300

    Добавьте файлы проекта.

---
+++ Lesson 14/Animal.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_14
+{
+    abstract class Animal
+    {
+        public abstract string Name { get; set; }// свойство без реализации
+
+        public Animal(string name)//конструктор
+        {
+            Name = name;
+        }
+
+        public abstract void Say();// Абстрактный. Нет тела и нет фигурных скобок              
+        public void ShowInfo() 
+        {
+            Console.WriteLine(Name);
+            Say();           
+        }
+    }
+
+
+
+    class Cat : Animal
+    {
+        string name;      
+        string say;
+        string size;
+
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public Cat(string name, string say, string size)
+           : base(name)
+        {
+            this.say = say;
+            this.size = size;
+        }
+        public override void Say()
+        {
+            Console.WriteLine(say);
+        }
+        public void ShowSize()// как вывести метод из производного класса???
+        {
+            Console.WriteLine(size);
+        }
+    }
+
+    class Dog : Animal
+    {      
+        string name;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public Dog(string name)
+           : base(name)
+        {           
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Гав");
+        }
+    }
+}
+
---
+++ Lesson 14/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Animal animal = new Cat ("Мурка", "Мяу", "Маленькая");
+            Cat cat = new Cat("Мурка", "Мяу", "Маленькая");
+            cat.ShowInfo();
+
+            Dog dog = new Dog("Бобик");
+            dog.ShowInfo(); 
+            Console.ReadKey();  
+        }
+    }
+}
